ee7cd14f8aee9fc858e660b2c8bf339a5be39eeb
ccofika <ccofika@gmail.com>
2025-10-09 19:18:57 +0200

    dodadjen env

--- config/db.js
+++ config/db.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // Check if MONGODB_URI is defined
+    if (!process.env.MONGODB_URI) {
+      throw new Error(
+        'MONGODB_URI is not defined! Please add it to your environment variables.\n' +
+        'For Render: Go to Dashboard > Your Service > Environment > Add MONGODB_URI'
+      );
+    }
+
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB Connected Successfully');
   } catch (error) {
